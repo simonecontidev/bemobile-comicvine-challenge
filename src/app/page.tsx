a9bb7d47c302904405b068af4d9fbd63f0fa099c
Simonecontidev <barriotropical@gmail.com>
2025-12-19 17:27:32 +0100

    chore: bootstrap next app with typescript
    Signed-off-by: Simonecontidev <barriotropical@gmail.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1 style={{ margin: 0 }}>BeMobile Challenge</h1>
+      <p style={{ marginTop: 10 }}>
+        Bootstrap complete. Next commits will add ComicVine API + UI.
+      </p>
+    </div>
+  );
+}
